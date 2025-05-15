8c5a8b1b031bb3baf417b235b5708d95896d9347
angelikadwi <dwiangelika@gmail.com>
2025-05-15 13:52:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,9 @@ st.title("tralala")
 st.write(
     "Love X-D."
 )
-st.image("views/450d080f-b15d-4a3f-afa0-10ca7d0552d8.jpeg")
+st.image("views/450d080f-b15d-4a3f-afa0-10ca7d0552d8.jpeg", width=200)
+st.write("\n")
+st.subheader("Angelika")
+st.write(
+    "Mari kita lihat X-D"
+)
