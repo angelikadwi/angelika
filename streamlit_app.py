d38b456bca8d5141521f5f5fc26ec6abedb4dfac
angelikadwi <dwiangelika@gmail.com>
2025-05-22 11:26:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,12 @@ st.subheader("Angelika")
 st.write(
     "Mari kita lihat X-D"
 )
+
+#with col1 :
+st.header ("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis Sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
